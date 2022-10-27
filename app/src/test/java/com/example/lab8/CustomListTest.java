64d9ad71acf3aeeccde9dddc6b37eb3149e7f156
UAWeiguoJiang <weiguo@ualberta.ca>
2022-10-27 17:01:07 -0600

    implement addCity()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -24,8 +24,7 @@ public class CustomListTest {
     /**
      * get the size of the list
      * increase the list by adding a new city
-     * check if our current size matches the initial size plus
-     one
+     * check if our current size matches the initial size plus one
      */
     @Test
     public void addCityTest(){
